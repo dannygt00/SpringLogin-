232ce8c8bacbe3239b0bf696a37e2122ec211cb7
danielramirez <dannyramirez917@gmail.com>
2021-04-09 16:16:21 -0400

    Fixed typos on application properties

--- src/main/java/registration/UserRegistrationController.java
+++ src/main/java/registration/UserRegistrationController.java
@@ -6,9 +6,7 @@
 package registration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
